85f427c8f4e32932585252c71c93189be5c09512
zmgPro <1067532194@qq.com>
2019-03-14 09:06:30 +0800

    start

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,20 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    image:"/images/p449619623.jpg",
+    title:"123",
+    comment:"aaa"
+  },
+
+  onLoad: function() {
+
+    }
+
+    // 获取用户信息
+
+  
+
+
+})
